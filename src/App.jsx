e145b4ecd6c963e23f1aab64a50f048f715d7ee7
PatrickNtl <natalskie@gmail.com>
2025-09-10 09:01:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Homepage from 'homepage';
 import LoginPage from './login-page';
 import SignupPage from './signup-page';
 import 
